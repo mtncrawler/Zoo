26632eb9b90666bf9d4db4a44eb56e0f35c5ee82
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-15 10:56:27 -0700

    created classes with all minimum requirements for abstract/virtual methods and props

--- lab05-zoo/classes/Fish.cs
+++ lab05-zoo/classes/Fish.cs
@@ -6,6 +6,11 @@ namespace lab05_zoo.classes
 {
     abstract class Fish
     {
+        public virtual int NumberOfGills { get; set; }
 
+        public virtual string MyUniqueness()
+        {
+            return "I can swim.";
+        }
     }
 }
--- lab05-zoo/classes/Human.cs
+++ lab05-zoo/classes/Human.cs
@@ -6,6 +6,14 @@ namespace lab05_zoo.classes
 {
     abstract class Human
     {
+        public virtual int NumberOfBeatingHearts { get; set; }
 
+
+        public abstract string MyMotto();
+
+        public virtual string GoalInLife()
+        {
+            return "I must ensure my survival.";
+        }
     }
 }
